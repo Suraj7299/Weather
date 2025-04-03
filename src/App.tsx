840fc854f8736fe356ddb2558a63a4b7047efd21
suraj7299 <surajbrnwal2424@gmail.com>
2025-04-03 20:36:37 +0530

    initialize project with Vite, React, and TypeScript setup; add basic components and styles

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <Button>
+          Click Me
+        </Button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
